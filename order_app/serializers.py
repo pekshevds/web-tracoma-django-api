d87cd81be35144abc4ec4b913ecc14a40d66724f
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-04-06 09:13:25 +0300

    create or update for contractor

--- order_app/serializers.py
+++ order_app/serializers.py
@@ -1,20 +1,52 @@
 from rest_framework import serializers
 from warehouse_app.serializers import CargoSerializer
+from order_app.models import Contractor
 
 
 class ContractorSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    name = serializers.CharField(max_length=150)
-    address1 = serializers.CharField(max_length=255)
-    address2 = serializers.CharField(max_length=255)
-    address3 = serializers.CharField(max_length=255)
-    phone1 = serializers.CharField(max_length=25)
-    phone2 = serializers.CharField(max_length=25)
-    phone3 = serializers.CharField(max_length=25)
+    id = serializers.UUIDField(read_only=True)
+    name = serializers.CharField(max_length=150, allow_blank=True)
+    address1 = serializers.CharField(max_length=255, allow_blank=True)
+    address2 = serializers.CharField(max_length=255, allow_blank=True)
+    address3 = serializers.CharField(max_length=255, allow_blank=True)
+    phone1 = serializers.CharField(max_length=25, allow_blank=True)
+    phone2 = serializers.CharField(max_length=25, allow_blank=True)
+    phone3 = serializers.CharField(max_length=25, allow_blank=True)
+
+    def create(self, validated_data):
+        return Contractor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for key in validated_data.keys():
+            if key == "id":
+                continue
+            if hasattr(instance, key):
+                setattr(instance, key,
+                        validated_data.get(key, getattr(instance, key))
+                        )
+        instance.save()
+        return instance
+
+    @staticmethod
+    def create_or_update_from_data(validated_data):
+        data = []
+        for item in validated_data:
+            id = item.get("id")
+            if id:
+                contractor = Contractor.objects.get(id=id)
+                contractor = ContractorSerializer(contractor, data=item)
+            else:
+                contractor = ContractorSerializer(data=item)
+            contractor.is_valid(raise_exception=True)
+            contractor.save()
+            data.append(contractor.instance)
+        serializer = ContractorSerializer(data=data, many=True)
+        serializer.is_valid()
+        return serializer
 
 
 class OrderSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
+    id = serializers.UUIDField(read_only=True)
     number = serializers.IntegerField()
     date = serializers.DateTimeField()
     sender = ContractorSerializer()
--- order_app/views.py
+++ order_app/views.py
@@ -14,7 +14,6 @@ from order_app.serializers import (
 class ContractorView(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
-    # permission_classes = [permissions.AllowAny]
 
     def get(self, request) -> Response:
         queryset = Contractor.objects.all()
@@ -23,11 +22,24 @@ class ContractorView(APIView):
                     "success": True}
         return Response(response)
 
+    def post(self, request) -> Response:
+        response = {"data": None,
+                    "success": False}
+        data = request.data.get("data")
+        if not data:
+            return Response(response)
+        serializer = ContractorSerializer(data=data, many=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer = ContractorSerializer.create_or_update_from_data(
+                validated_data=data)
+            response = {"data": serializer.data,
+                        "success": True}
+        return Response(response)
+
 
 class OrderView(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
-    # permission_classes = [permissions.AllowAny]
 
     def get(self, request) -> Response:
         queryset = Order.objects.all()
